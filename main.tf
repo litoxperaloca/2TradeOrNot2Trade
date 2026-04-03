e47d93db33041dd2763dc4d363b55486d239ab41
Pablo Pignolo <pablo.pignolo@gmail.com>
2026-04-03 17:22:08 -0300

    Create main.tf file with Azure infrastructure configuration

---
+++ main.tf
@@ -0,0 +1,136 @@
+# Terraform Configuration for Azure Infrastructure
+
+provider "azurerm" {
+  features {}
+}
+
+# Resource Group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+# Security Group
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+# Blob Storage
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestoracc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier            = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Cosmos DB
+resource "azurerm_cosmosdb_account" "example" {
+  name                = "example-cosmosdb"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  offer_type         = "Standard"
+  consistency_policy {
+    consistency_level = "Session"
+  }
+}
+
+# SQL Database
+resource "azurerm_sql_server" "example" {
+  name                         = "example-sqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password  = "P@ssw0rd1234"
+}
+
+resource "azurerm_sql_database" "example" {
+  name                = "exampledb"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  server_name        = azurerm_sql_server.example.name
+  sku { 
+    name     = "S0"
+    tier     = "Standard"
+    capacity = 10
+  }
+}
+
+# Azure Functions
+resource "azurerm_function_app" "example" {
+  name                       = "example-functionapp"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  app_service_plan_id       = azurerm_app_service_plan.example.id
+  storage_account_name      = azurerm_storage_account.example.name
+  os_type                    = "linux"
+}
+
+# App Service
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier     = "Standard"
+    size     = "S1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "example-appservice"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+}
+
+# Key Vault
+resource "azurerm_key_vault" "example" {
+  name                = "example-keyvault"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  tenant_id          = "YOUR_TENANT_ID"
+  sku_name           = "standard"
+}
+
+# Application Insights
+resource "azurerm_application_insights" "example" {
+  name                = "example-appinsights"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  application_type    = "web"
+}
+
+# Log Analytics Workspace
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = "example-loganalytics"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+# Monitoring Alerts
+resource "azurerm_monitor_metric_alert" "example" {
+  name                = "example-alert"
+  resource_group_name = azurerm_resource_group.example.name
+  criteria {
+    metric_namespace = "Microsoft.Compute/virtualMachines"
+    metric_name      = "Percentage CPU"
+    aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 75
+  }
+}
+
